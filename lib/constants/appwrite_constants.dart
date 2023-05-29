2583ea397b0290bd194b2beb2389a71481aa608c
james <jamessupermail@gmail.com>
2023-05-30 00:47:02 +0800

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '6474d2d6aa4e2aad0a0e';
+  static const String projectId = '6474cb9375dafdc59937';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+}
