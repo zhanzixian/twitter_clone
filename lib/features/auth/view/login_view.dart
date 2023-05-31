1a85d6d2cf9a36dbb3d0787d18bc1cea6470c809
james <jamessupermail@gmail.com>
2023-05-31 23:45:17 +0800

    feat: login_view

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/common/rounded_small_button.dart';
 import 'package:twitter_clone/constants/ui_constants.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,16 +14,57 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   final appbar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbar,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                AuthField(
+                  controller: emailController,
+                  hintText: 'Email',
+                ),
+                const SizedBox(height: 25),
+                AuthField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                ),
+                const SizedBox(height: 40),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: RoundedSmallButton(
+                    onTap: () {},
+                    label: 'Done',
+                  ),
+                ),
+                const SizedBox(height: 40),
+                RichText(
+                    text: TextSpan(
+                        text: "Don't have an account?",
+                        style: const TextStyle(fontSize: 16),
+                        children: [
+                      TextSpan(
+                          text: ' Sign up',
+                          style: const TextStyle(
+                              color: Pallete.blueColor, fontSize: 16),
+                          recognizer: TapGestureRecognizer()..onTap = () {}),
+                    ]))
+              ],
+            ),
           ),
         ),
       ),
